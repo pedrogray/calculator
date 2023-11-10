2a6d368ab0a9b8f37801d260f1f7d4f81e2c8161
Pedro Gray <144771627+pedrogray@users.noreply.github.com>
2023-11-10 13:10:55 -0700

    Added basic logic for operations. Finished adding event listeners for buttons.

--- script.js
+++ script.js
@@ -15,33 +15,76 @@ sumButton = document.querySelector("#sumButton")
 sumButton.addEventListener("click", function () { 
     add()
 })
+
+minusButton = document.querySelector("#minusButton")
+minusButton.addEventListener("click", function(){
+    substract()
+})
+
 equalsButton = document.querySelector("#equalsButton")
 equalsButton.addEventListener("click", function() {
-operate()
+operate();
+})
+
+clearButton = document.querySelector("#clearButton");
+clearButton.addEventListener("click", function() { 
+    clear();
+})
+
+divideButton = document.querySelector("#divideButton");
+divideButton.addEventListener("click", function() { 
+    divide();
+})
+
+multiplyButton = document.querySelector("#multiplyButton");
+multiplyButton.addEventListener("click", function() {
+    multiply()
 })
 
 
+
+
 function add() {
-    if (operator !== '') {operate()}
+    if (operator !== '') {operate()
+        number1 = parseInt(screenContent.textContent)
+        operator= "";}
     else {
     number1 = parseInt(screenContent.textContent)
     operator= "+";
     screenContent.textContent = "";}
 }
 
-function substract(number1,number2) {
-    let sub = number1-number2;
-    return console.log(sub);
+function substract() {
+    if (operator !== '') {operate()
+        number1 = parseInt(screenContent.textContent)
+        operator= ""}
+    else {
+        number1 = parseInt(screenContent.textContent);
+        operator="-";
+        screenContent.textContent=''
+    }
 }
 
-function multiply (number1,number2) {
-    let mult = number1*number2;
-    return console.log(mult);
+function multiply () {
+    if (operator !== '') {operate()
+        number1 = parseInt(screenContent.textContent)
+        operator= ""}
+    else {
+        number1 = parseInt(screenContent.textContent);
+        operator="*";
+        screenContent.textContent=''
+    }
 }
 
-function divide (number1,number2) {
-    let division = number1/number2;
-    return console.log(division)
+function divide () {
+    if (operator !== '') {operate()
+        number1 = parseInt(screenContent.textContent)
+        operator= ""}
+    else {
+        number1 = parseInt(screenContent.textContent);
+        operator="/";
+        screenContent.textContent=''
+    }
 }
 
 function operate() {
@@ -52,4 +95,35 @@ function operate() {
         number1 = result;
         number2 = ''
         operator= ''};
+    if (operator == "-") {
+        let result = number1-number2;
+        screenContent.textContent = result;
+        number1 = result;
+        number2 = ''
+        operator = ''
+    }
+    if (operator =="*") {
+        let result = number1*number2;
+        screenContent.textContent = result;
+        number1 = result;
+        number2 = ''
+        operator = ''
+    }
+    if (operator == "/") {
+        if (number2 == 0) {screenContent.textContent = "LMFAO."}
+        else{
+        let result = number1/number2;
+        screenContent.textContent=result;
+        number1=result;
+        number2 = ''
+        operator = ''
+    }
+    }
+}
+
+function clear() {
+    number1=''
+    number2=''
+    operator=''
+    screenContent.textContent = ''
 }
