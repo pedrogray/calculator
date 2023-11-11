8e6eb3d30d20d4ebf60b6f08d6e7bffff8a0af4d
Pedro Gray <144771627+pedrogray@users.noreply.github.com>
2023-11-11 15:24:21 -0700

    Added decimal capabilities to the calculator. Allowed only one dot to be used.

--- script.js
+++ script.js
@@ -1,15 +1,23 @@
-let firstNumber = ''
-let secondNumber = ''
 let operator = ''
+let screenContent = document.querySelector(".screenContent")
+screenContent.textContent=""
+
 
 let numberButtons = []
 for (let i=0; i<=9; i++) {
+    
     numberButtons[i] = document.querySelector("#number"+i)
     numberButtons[i].addEventListener("click", function() { screenContent.textContent += [i]})
 }
 
-let screenContent = document.querySelector(".screenContent")
-screenContent.textContent=""
+let dotButton = document.querySelector("#dot")
+dotButton.addEventListener("click", function(){
+    if (screenContent.textContent.indexOf('.') == -1) {
+        screenContent.textContent += "."
+    }
+})
+
+
 
 sumButton = document.querySelector("#sumButton")
 sumButton.addEventListener("click", function () { 
@@ -46,20 +54,20 @@ multiplyButton.addEventListener("click", function() {
 
 function add() {
     if (operator !== '') {operate()
-        number1 = parseInt(screenContent.textContent)
+        number1 = parseFloat(screenContent.textContent)
         operator= "";}
     else {
-    number1 = parseInt(screenContent.textContent)
+    number1 = parseFloat(screenContent.textContent)
     operator= "+";
     screenContent.textContent = "";}
 }
 
 function substract() {
     if (operator !== '') {operate()
-        number1 = parseInt(screenContent.textContent)
+        number1 = parseFloat(screenContent.textContent)
         operator= ""}
     else {
-        number1 = parseInt(screenContent.textContent);
+        number1 = parseFloat(screenContent.textContent);
         operator="-";
         screenContent.textContent=''
     }
@@ -67,10 +75,10 @@ function substract() {
 
 function multiply () {
     if (operator !== '') {operate()
-        number1 = parseInt(screenContent.textContent)
+        number1 = parseFloat(screenContent.textContent)
         operator= ""}
     else {
-        number1 = parseInt(screenContent.textContent);
+        number1 = parseFloat(screenContent.textContent);
         operator="*";
         screenContent.textContent=''
     }
@@ -78,33 +86,33 @@ function multiply () {
 
 function divide () {
     if (operator !== '') {operate()
-        number1 = parseInt(screenContent.textContent)
+        number1 = parseFloat(screenContent.textContent)
         operator= ""}
     else {
-        number1 = parseInt(screenContent.textContent);
+        number1 = parseFloat(screenContent.textContent);
         operator="/";
         screenContent.textContent=''
     }
 }
 
 function operate() {
-    number2 = parseInt(screenContent.textContent)
+    number2 = parseFloat(screenContent.textContent)
     if (operator == "+") {
         let result = number1+number2;
-        screenContent.textContent = result;
+        screenContent.textContent = result.toFixed(2);
         number1 = result;
         number2 = ''
         operator= ''};
     if (operator == "-") {
         let result = number1-number2;
-        screenContent.textContent = result;
+        screenContent.textContent = result.toFixed(2);
         number1 = result;
         number2 = ''
         operator = ''
     }
     if (operator =="*") {
         let result = number1*number2;
-        screenContent.textContent = result;
+        screenContent.textContent = result.toFixed(2);
         number1 = result;
         number2 = ''
         operator = ''
@@ -113,7 +121,7 @@ function operate() {
         if (number2 == 0) {screenContent.textContent = "LMFAO."}
         else{
         let result = number1/number2;
-        screenContent.textContent=result;
+        screenContent.textContent=result.toFixed(2);
         number1=result;
         number2 = ''
         operator = ''
